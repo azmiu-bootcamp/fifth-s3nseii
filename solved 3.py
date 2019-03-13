2336b8db849b27de72d160bcd3d994d744c6c925
s3nseii <47414629+s3nseii@users.noreply.github.com>
2019-03-13 18:52:07 +0400

    Create solved 3.py

---
+++ solved 3.py
@@ -0,0 +1,13 @@
+def lone_sum(a,b,c):
+    a = int(input('a -nin qiymetini daxil edin:'))
+    b= int(input('b -nin qiymetini daxil edin:'))
+    c = int(input('a -nin qiymetini daxil edin:'))
+    d = a + b + c
+    if a==b:
+        print('Cemlene bilmir')
+    elif b==c:
+        print('Cemlene bilmir')
+    elif a==c:
+        print ('Cemlene bilmir')
+    else:
+        print(c)
